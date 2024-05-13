43d4988434d3c75b43b57b1d60731a2699d9ec45
rinceseptriana <rinceparhusip29@gmail.com>
2024-05-13 10:52:44 +0700

    hello world & variable

---
+++ index.js
@@ -0,0 +1,1 @@
+console.log('programming batch 4')
---
+++ variable.js
@@ -0,0 +1,20 @@
+// const namaPelanggan = 'Jordan'
+// const umur = 32
+// const sudahMenikah = true
+// const barangOrderan = ['baju', 'celana', 'sepatu']
+// const alamatPelanggan = {
+//     namajala: 'jl.setiabudi',
+//     nomorjalan: 5
+// }
+
+// console.log(`Nama: ${namaPelanggan}`);
+// console.log(`Umur: ${umur}`);
+// console.log(`Sudah Menikah: ${sudahMenikah}`);
+// console.log(`Barang Orderan: ${barangOrderan}`);
+// console.log(`Alamat pelanggan: ${JSON.stringify(alamatPelanggan, null, 2)}`);
+
+// trim -> hapus spasi (nama.trimEnd()
+// delete data paling akhir -> angka.pop()
+
+const tgl = new Date();
+console.log(tgl);
